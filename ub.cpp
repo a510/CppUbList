cc9b29b86a0425022b19379af19b4ad805e00c72
Ahmad Ibrahim <ahmad.cdev@gmail.com>
2018-11-20 17:19:42 +0000

    Update ub.cpp

--- ub.cpp
+++ ub.cpp
@@ -1,5 +1,18 @@
 // Source: https://en.cppreference.com/w/cpp/language/ub
-
-int foo(int x) {
+int foo1(int x) {
     return x+1 > x; // either true or UB due to signed overflow
+}
+
+// Source: https://en.cppreference.com/w/cpp/language/ub
+void foo2() {
+    bool p; // uninitialized local variable
+    if(p) // UB access to uninitialized scalar
+        std::puts("p is true");
+}
+
+https://twitter.com/shafikyaghmour/status/1064366507323478016
+void foo3() {
+    int n = 1;
+    auto p = std::minmax(n, n+1);
+    printf("%d%d", p.first, p.second);
 }
